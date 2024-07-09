91ca4177a68e129f1335f66107256ec307d26fda
Jupiter Group <ourjupitergroup@gmail.com>
2024-07-09 12:23:00 -0400

    Update index.html, script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const terminalBody = document.getElementById('terminalBody');
-    const defaultMessage = 'AS-WOS [Version 6.44.52.37] (c) Aurorasoft. All Rights Reserved.';
+    const defaultMessage = 'Jupiter WebTerminal [Version 6.44.52.39] (c) Jupiter Group. All Rights Reserved.';
     
     const defaultOutput = document.createElement('div');
     defaultOutput.textContent = defaultMessage;
     terminalBody.appendChild(defaultOutput);
 });
 
-window.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.key === 's') {
-        event.preventDefault();
-        window.location.href = './safemode.html';
-    }
-});
+// window.addEventListener('keydown', function(event) {
+//    if (event.ctrlKey && event.key === 's') {
+//        event.preventDefault();
+//        window.location.href = './safemode.html';
+//    }
+// });
 
 const userInput = document.getElementById('userInput');
 const commandHistory = [];
@@ -90,22 +90,22 @@ function executeCommand(command) {
         parts.splice(0, 2, 'weather_find');
     }
 
-    if (cmd === 'fork' && parts[1] === 'bomb') {
-        parts.splice(0, 2, 'forkbomb');
-    }
+    // if (cmd === 'fork' && parts[1] === 'bomb') {
+    //    parts.splice(0, 2, 'forkbomb');
+    // }
 
 
     switch (parts[0]) {
         case 'about':
-            output.innerHTML = `AS-WOS Version 6.44 <br> (C)Copyright Aurorasoft 2021-2024`;
+            output.innerHTML = `Jupiter WebTerminal Version 6.44 <br> (C)Copyright Jupiter Group 2021-2024`;
         break;
     
         case 'askey':
-            output.textContent = 'AS-WOS is distributed by Aurorasoft. The current key is b6842ded-ef40-44e8-9efd-47260604dc97';
+            output.textContent = 'Jupiter WebTerminal is distributed by Jupiter Group. The current key is b6842ded-ef40-44e8-9efd-47260604dc97';
         break;
     
         case 'basic':
-            output.innerHTML = `AS-WOS is a text-based web desktop. Type 'command' for a list of possible commands.`;
+            output.innerHTML = `Jupiter WebTerminal is a text-based web desktop. Type 'command' for a list of possible commands.`;
         break;
     
         case 'break':
@@ -113,7 +113,7 @@ function executeCommand(command) {
         break;
 
         case 'checksystem':
-            output.innerHTML = `AS-WOS is: FUNCTIONAL.`;
+            output.innerHTML = `Jupiter WebTerminal is FUNCTIONAL.`;
         break;
     
         case 'command':
@@ -121,7 +121,7 @@ function executeCommand(command) {
         break;
     
         case 'copyright':
-            output.innerHTML = 'AS-WOS is distributed by Aurorasoft. Copyright (c) 2024 Aurorasoft.';
+            output.innerHTML = 'Jupiter WebTerminal is distributed by Jupiter Group. Copyright (c) 2021-2024 Jupiter Group.';
         break;
     
         case 'date':
@@ -160,7 +160,7 @@ function executeCommand(command) {
         break;
     
         case 'dir':
-            output.innerHTML = `D`;
+            output.innerHTML = `A`;
         break;
     
         case 'echo':
@@ -182,14 +182,13 @@ function executeCommand(command) {
             }
         break;
 
-        case 'forkbomb':
-            output.innerHTML = `EZSXZyb5JizoZXXPiEzBZrmcfheYqJmg\nOfjJQUXuoQEOO5OEUKfo0Qp4xaEQ1wSW\nxM0PTdQYSvUumF7CJI1Jig5v52IN499l\nAsmvV5ByxyhEOgYDTROFZHQHzwAUA1Gx\nwCzOSXYvfdGhOuBSgcuuXhwCrIJXwciv\nNka61TdmxjrwCUFnggx45G5eGIPW6oDw\nr5On9mFmuURyZI6HXR5E8XbmHaxJ9ZIm\naBFf3FwJ4T7oRGY3zyxDIxjP3IyoEHrq\nndSJaE8wqgPeTwRz2sXbk2vR80aQ1Vwe\nbnRgQcnkinCT5qQ65ELTnJzR0vJHc87x\nuqfvSAJPl1lppOmQOwZyOBHSiT8PIdfX\niq7uGc8Wck0cG8CKBHVShl34Zm1vqQhe\n0nirJXTTy80H36hpAIo2UOE9yswbQSls\nENYAVn83AnBcRVrNMYI0ebsFc7f4I2Tm\nGsewDHIzwV2Rooljk413yhWWpc3gqy5G\ngVpicTbG6sLzYPljw4owuzsu8iLKv0f9\nL2fLQOJi2hL9qghTMbbXWcb8gPDUd4tQ\n5nGbkZzm48AzLAfgisefAV6wclkBDh3Q\n7nZW6qW0NX6stUvlGmdayTsPSTvGsAeH\niEbgFvFRgrgDHHALk6pyKmpWYKseS6Ao\nz4WQM8OwoeJe16INcMvAU6PHvWumyK0S\nXiGBEzzHHnuvZkLdEmZpfQIQmJ7sUY5g\nyKV4ERGI3eLMoTPbLFAdfpqFYzvwFq4v\nt8pvbSBFkIC16OhWBFwiBK30zxZIItUl\nTS5ieAgLszbVcBryifLMG489xSUTqzPq\nvlg77gnMCc26P5XOPbxJGNhZKgyAww20\n3Ms07qFEdYgYtEfqzSXejw46kPf7kVE8\nRVWQm32HIwXgc25elP1Z1IByFhj0krZg\n9fOprWvjWZ0kn2Db1fcTFLEP2MjIypHL\njwhGl8xML33N2GrO9xIBLMYQns2nc59B\nciN83lNNvwfz3ZpPdUmwPZ91Lioxj8ZO\nTC8csbwR4mJO4iwgIutCk2FuG7I4E2Ra\nWP8SsZUVYdiuWEj7Iwme9eUVAg5lNE3C\nxZ9WtPh592MjRxK5EHgp4xaUqgOAEX7s\nMFGxHTYQysBijDn0Eu5qeP2HDSNMsbfD\nt3zC4GtR2nZfePZEQb0wIfYJIjcGovmv\npYydzS1SUt3z9xk5yA9T1yC0WPye6opg\nraqJz3ExRfl34BFrU9WXHCdBpQ5BOJpt\nSTQEuSoZMCu9ycuNwz3N0shqyH5euAuz\nGchMrX5gUipN5Vvzd3QpZWCEb85BxHxW\nxxW0D7Uy1VLaHY0ikBYbBDam8RLYfvax\nHTAzCeVJPUZ1XIraVlbN2LERq5Hm7Uyx\ntioSqDEPjM0T5l6VAXRYVerQJUTfy2fP\nILEpzPThQvaXZfOZbwnDt8pXllkmnUEu\n2HAYiKmiYyPWwJFEkeFuTInLoOrHcrmZ\nFHazWMQYhtdNVgtXiFk1E6jdiZrojldJ\nnAVYkj2ipe1WA1gqKQrDG96YVUItDEla\nW8S4Hf9uO0IriHrKb10LxdsZR6obsvG7\nObLMC3ydQIo38b5sgyxzCtmZT3VNydqX\ntgiq7wMreCwFYwOw7pqKk2y0z8CaAMWl\nqeWkeG6SyiqFqEk6n0Rf6KjyIoWnjTJT\nU3u8GWOahsoZDUkMMmW7PuMV68NMpO0h\n7alFN0LrEHqhzsoiJkbodTlon1WlyXmn\ncnUv8br1Bth0y4mE1325VJxm7RigY8pO\nv5Y0Lem5Zx79kQcjyQrdGG7brdbfGRLT\n0SJ1D8ZlBFRoXxc7RppsQHDX11qLhuMK\nbZppsqBN2qodRSI5h9kuRjHaXGh8i7Jp\najDthRLuobSWpNVkltiZD42tH4AP7QLh\nDtIKReQYheondcHvOLtQTRMuq1vV27mU\n1IsIFMqAYd8ooQxgp3UNZchUUUfZB97G\n5SI5yn5cQcVIiXHbh3JmSyscNy86vPsb\nYJ1xzV6GprBsoLlmNfPiITeTnmQLvDQ5\nZqp7ZzdezaROOsnumLV7AZGHxryVfEW0\nn5mninpod3pMUCbBrPUSP8Iiup9nltWi\nKd3DNEkryUqZN6PpR4EQfDkmx47AMCvf\nVZoelrXNwHvv1q7k9kbcwY1MUoOio8l9\nhd5sjAPGTcWxYiY5Xxfmh07nPt3PtzoF\ngEA1i8X1mfWjVLHlxo8eERIzz0oE3WVY\nPGC5kQK48dGiLllx7KvAxAOEGIffIgfY\nmE17bnR2emDiTPKFMW8T9TISNmnVHr5i\nC27bj5RJPsYWZ2jUWpEnZdJRq325KbxR\nUiCUiWwJBPxI2UiMfoSsEwHtS0Sf4RyR\n5NnPk7IUFbDOImZnttkHWca7Uu7AhGjp\nRKyLGQfQGvVtKhIhLlPaIMN5talRUk0D\nQMGN87cHtMidadjc63SJrAXGor6FqamG\nddhwjsbpnLze9ExHxk7NH8mE3CbHr7OY\nlUJhRdsfwJ5JNEhEHcJzLT4NfOtXxI9N\nCPKQMXIEDg70y4RFLHkVCVcLy93nRPfn\nKKIT7clBnMrgW0LKeXpOQfVS6sXPn6a9\nOue1IsLB7oToTvLuLNmRNrKp2ms1cTjY\n9zW078kL85eBzy0IV4nUmdXEcPieGOeK\ngM8VE2xdNhsGp7r6fk0pzr8DA5zkMfFE\njRdJZ7Qi57CNVOkZ9LRWB9QbtCHkJaIq\npj8jjvDz2DAg5FG0rc5cFvR4HzRTe6XA\nxo9O9QiAtYtI0Ksf9OJ3oZ7MShXiWmWR\n1YWnhpMqP4czASFsiMKU41rwRucCseih\nlJQXu813Y8gjlpwAWtFHMkAsHx1FlI0t\nw0J4SBeQqBzl3SjqpHR6UhtfDUAjFtAr\nSZZAboeCYQnOaghsl4ZftopiLDaGnqZR\n07hMhBn5ZeDlnkbdBJUIJlzPIRKBPUbE\nCXxc4UzEcSWxDABDDBGsn8F4UqASmCWc\ngcDDgCDi6CEdLKSstlbV1jXF7BNVc3e1\nnGAcRRv6NsdTwngPahvPv2H7H6Qn0QJq\nS8BfGPkiXOwBxokd9raCE4HGwar1d30A\nQxVLAulq4PrawGfdzVMkwCPy3h21b19X\nk4eRr0hzwWew6REAey91N8qHEGaUATo5\nyJZVrEvYcqbG4BOGV9wWJftCuoEpPy35\n0jgZizZJIQ2K2B6pZRfVyjti8Yq8JXyQ\nL43GztixgvJWAWSTM1MRt2XHFbn2mZ1w\nl11o3WRLr5mX3Aemx7g3sC6h2MaWNv51`;
-            setTimeout(() => {
-                window.location.href = './error.html';
-            }, 10000);
-            break;
-        break;
-    
+        // case 'forkbomb':
+        //    output.innerHTML = `lol`;
+        //    setTimeout(() => {
+        //        window.location.href = './error.html';
+        //    }, 10000);
+        // break;
+
         case 'goto':
             if (parts.length < 2) {
                 output.textContent = 'Usage: goto <url>';
@@ -234,7 +233,7 @@ function executeCommand(command) {
         break;
     
         case 'repo':
-            window.location.href = 'https://github.com/BarclaySoftware/BT-WOS';
+            window.location.href = 'https://github.com/thejupitergroup/WebTerminal';
         break;
     
         case 'time':
@@ -243,11 +242,11 @@ function executeCommand(command) {
         break;
     
         case 'verify':
-            output.textContent = `AS-WOS is distributed by Aurorasoft. Copyright (c) 2024 Aurorasoft.`;
+            output.textContent = `Jupiter WebTerminal is distributed by Jupiter Group. Copyright (c) 2021-2024 Jupiter Group.`;
         break;
     
         case 'version':
-            output.textContent = `AS-WOS Version 6.44`;
+            output.textContent = `Jupiter WebTerminal Version 6.44`;
         break;
     
         case 'weather':
